56bfeb06f0356b42a0417ce9a4fd364d1a4a8128
abeer <abeer.sewak@gmail.com>
2020-04-24 16:46:07 -0500

    updated signal to create 10 frames per game

--- app/scoring_app/signals.py
+++ app/scoring_app/signals.py
@@ -6,7 +6,12 @@ from .models import Game, Frame
 @receiver(post_save, sender=Game)
 def create_frame(sender, instance, created, *args, **kwargs):
     if created:
-        Frame.objects.create(game_id=instance, frame_no=5)
+        frames = [
+            Frame(game_id=instance, frame_no=frame_no) for frame_no in range(1, 11)
+        ]
+        Frame.objects.bulk_create(frames)
+
+        # Frame.objects.create(game_id=instance, frame_no=5)
 
 
 # @receiver(post_save, sender=Game)
