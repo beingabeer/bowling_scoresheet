8be73dfc9d8c078676f38b847e4e75977572f4ee
abeer <abeer.sewak@gmail.com>
2020-04-24 10:07:22 -0500

    unregister groups

--- app/scoring_app/admin.py
+++ app/scoring_app/admin.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import Group
 from django.contrib import admin
 from .models import Player, Game, Frame
 
+admin.site.unregister(Group)
 admin.site.register(Player)
 admin.site.register(Game)
 admin.site.register(Frame)
