c7295a6871bcec358188f3ed58d34c3a995d58a5
abeer <abeer.sewak@gmail.com>
2020-04-24 16:48:07 -0500

    cleaned signals

--- app/scoring_app/signals.py
+++ app/scoring_app/signals.py
@@ -10,10 +10,3 @@ def create_frame(sender, instance, created, *args, **kwargs):
             Frame(game_id=instance, frame_no=frame_no) for frame_no in range(1, 11)
         ]
         Frame.objects.bulk_create(frames)
-
-        # Frame.objects.create(game_id=instance, frame_no=5)
-
-
-# @receiver(post_save, sender=Game)
-# def save_frame(sender, instance, *args, **kwargs):
-#     instance.frame.save()
