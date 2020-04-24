afd8b77fa80fa1b4089b42f7317bc055260b1a38
abeer <abeer.sewak@gmail.com>
2020-04-24 16:30:30 -0500

    updated models

--- app/scoring_app/models.py
+++ app/scoring_app/models.py
@@ -1,25 +1,43 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+import uuid
+from django.db.models.signals import pre_save, post_save
 
 
 class Player(models.Model):
     player_id = models.AutoField(primary_key=True)
     player_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.player_name
+
 
 class Game(models.Model):
     game_id = models.AutoField(primary_key=True)
-    player_name = models.ForeignKey(Player, on_delete=models.CASCADE)
+    player_name = models.ForeignKey(
+        Player, on_delete=models.CASCADE, related_name='player')
     in_progress = models.BooleanField(default=True)
     is_over = models.BooleanField(default=False)
     final_score = models.IntegerField(default=0)
 
+    def __str__(self):
+        return f"Game {self.game_id} - ({self.player_name})"
+
 
 class Frame(models.Model):
     frame_id = models.AutoField(primary_key=True)
-    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
-    frame_no = models.IntegerField(choices=list(
-        zip(range(1, 11), range(1, 11))), unique=True)
-    roll_one = models.IntegerField()
-    roll_two = models.IntegerField()
+    # frame_id = models.UUIDField(
+    #     primary_key=True, default=uuid.uuid4, editable=False)
+    game_id = models.ForeignKey(
+        Game, on_delete=models.CASCADE, related_name='game')
+    # frame_no = models.IntegerField(choices=list(
+    #     zip(range(1, 11), range(1, 11))))
+    frame_no = models.IntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)])
+    roll_one = models.IntegerField(default=0)
+    roll_two = models.IntegerField(default=0)
     roll_three = models.IntegerField(default=0)
     roll_four = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.game_id} Frame-{self.frame_no}"
