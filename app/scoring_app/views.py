c493671feb56de51c3ef1745af911a241f7f738e
abeer <abeer.sewak@gmail.com>
2020-04-24 22:57:45 -0500

    added crud view functions

--- app/scoring_app/views.py
+++ app/scoring_app/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Player, Game, Frame
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.generic import CreateView
 
-# Create your views here.
+
+def index(request):
+    game_list = Game.objects.all()
+    context = {'game_list': game_list}
+    return render(request, 'scoring_app/index.html', context)
+
+
+def game_detail(request, pk):
+    game = Game.objects.get(pk=pk)
+    context = {'game': game}
+    return render(request, 'scoring_app/detail.html', context)
+
+
+def game_delete(request, pk):
+    game = Game.objects.get(pk=pk)
+    game.delete()
+    return redirect('index')
+
+
+class GameCreateView(CreateView):
+    model = Game
+    template_name = "scoring_app/add_game.html"
+    fields = ("player_name",)
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+        # return HttpResponseRedirect(self.get_success_url())
+
+
+# def create_game(request):
+#     if request.method == "POST":
+#         player_name = request.POST.get('player_name')
+#         Game.object.create(player_name=player_name)
