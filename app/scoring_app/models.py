81a669485af6d929bdb634509a6e0a915531cb9f
abeer <abeer.sewak@gmail.com>
2020-04-25 20:58:50 -0500

    fixed import issue in models.py

--- app/scoring_app/models.py
+++ app/scoring_app/models.py
@@ -2,6 +2,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils import timezone
 from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
 
 
 class Player(models.Model):
