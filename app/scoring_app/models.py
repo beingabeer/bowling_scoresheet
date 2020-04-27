dfc6adbab2943212571d773f5913dc46f4dbf5c1
abeer <abeer.sewak@gmail.com>
2020-04-26 22:47:59 -0500

    add only unique player names

--- app/scoring_app/models.py
+++ app/scoring_app/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 
 class Player(models.Model):
     player_id = models.AutoField(primary_key=True)
-    player_name = models.CharField(max_length=50)
+    player_name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.player_name
