5de7c0b8a5f3682cdb23d990a4ecd5d4aa1d4a76
abeer <abeer.sewak@gmail.com>
2020-04-26 20:05:53 -0500

    update backend with user input for pins knock

--- app/scoring_app/forms.py
+++
@@ -1,5 +0,0 @@
-# from django import forms
-
-
-# class GameCreationForm(forms.Form):
-#     player_name = forms.CharField(required=True)
--- app/scoring_app/views.py
+++ app/scoring_app/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Player, Game, Frame
 from django.http import HttpResponseRedirect, HttpResponse
-from django.views.generic import CreateView
-# from .forms import GameCreationForm
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 
@@ -18,7 +17,33 @@ def game_list(request):
 
 def game_detail(request, pk):
     game = Game.objects.get(pk=pk)
-    context = {'game': game}
+    frame_list = game.frame_set.all()
+    frame_active_count = frame_list.filter(frame_is_active=True).count()
+    context = {'game': game, 'frame_list': frame_list,
+               'frame_active_count': frame_active_count}
+
+    if request.method == "POST":
+        roll_one = request.POST.get('roll_one')
+        roll_two = request.POST.get('roll_two')
+        current_frame = Frame.objects.get(
+            game_id=game, frame_is_active=True)
+        current_frame.roll_one = roll_one
+        current_frame.roll_two = roll_two
+        current_frame_num = current_frame.frame_no
+        current_frame.frame_is_active = False
+        current_frame.save()
+        if current_frame.frame_no+1 <= 10:
+            next_frame = Frame.objects.get(
+                game_id=game, frame_no=current_frame_num+1)
+            next_frame.frame_is_active = True
+            next_frame.save()
+
+        else:
+            game.in_progress = False
+            game.save()
+
+        return render(request, 'scoring_app/detail.html', context)
+
     return render(request, 'scoring_app/detail.html', context)
 
 
@@ -48,3 +73,5 @@ class PlayerCreateView(CreateView):
         self.object = form.save(commit=False)
         self.object.save()
         return redirect('game-list')
+
+
