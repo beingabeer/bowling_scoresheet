b07d79fad2678b8b39bc753b2c67b6c47b51968e
abeer <abeer.sewak@gmail.com>
2020-04-27 22:42:59 -0500

    updated views logic to accept valid roll inputs only

--- app/scoring_app/views.py
+++ app/scoring_app/views.py
@@ -23,23 +23,32 @@ def game_detail(request, pk):
     context = {'game': game, 'frame_list': frame_list,'frame_active_count': frame_active_count}
 
     if request.method == "POST":
-        roll_one = request.POST.get('roll_one')
-        roll_two = request.POST.get('roll_two')
-        current_frame = Frame.objects.get(game_id=game, frame_is_active=True)
-        current_frame.roll_one = roll_one
-        current_frame.roll_two = roll_two
-        current_frame_num = current_frame.frame_no
-        current_frame.frame_is_active = False
-        current_frame.save()
-        if current_frame.frame_no+1 <= 10:
-            next_frame = Frame.objects.get(
-                game_id=game, frame_no=current_frame_num+1)
-            next_frame.frame_is_active = True
-            next_frame.save()
+        roll_one = int(request.POST.get('roll_one'))
+        roll_two = int(request.POST.get('roll_two'))
+
+        if roll_one + roll_two > 10:
+            # include error message here
+            return render(request, 'scoring_app/detail.html', context)
+        elif roll_one == 10 and roll_two > 0:
+            # include error message here
+            return render(request, 'scoring_app/detail.html', context)
 
         else:
-            game.in_progress = False
-            game.save()
+
+            current_frame = Frame.objects.get(game_id=game, frame_is_active=True)
+            current_frame.roll_one = roll_one
+            current_frame.roll_two = roll_two
+
+            current_frame.frame_is_active = False
+            current_frame.save()
+            if current_frame.frame_no+1 <= 10:
+                next_frame = Frame.objects.filter(game_id=game, frame_no__gt=current_frame.frame_no).order_by('frame_no').first()
+                next_frame.frame_is_active = True
+                next_frame.save()
+
+            else:
+                game.in_progress = False
+                game.save()
 
         return render(request, 'scoring_app/detail.html', context)
 
