dfc1489cf02fdcac532a046f02c812599963d521
abeer <abeer.sewak@gmail.com>
2020-04-29 00:14:40 -0500

    updated api

--- app/scoring_app/api/serializers.py
+++ app/scoring_app/api/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from scoring_app.models import Player, Game, Frame
 
 
+class PlayerCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ('player_name',)
+
+
 class PlayerListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
@@ -12,13 +18,19 @@ class PlayerDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = '__all__'
+        
+
+
+class GameCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ('player_id',)
 
 
 class GameListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = '__all__'
-
 
 
 class GameDetailSerializer(serializers.ModelSerializer):
--- app/scoring_app/api/views.py
+++ app/scoring_app/api/views.py
@@ -1,6 +1,15 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView, CreateAPIView
 from scoring_app.models import Player, Game, Frame
-from .serializers import PlayerListSerializer, PlayerDetailSerializer, GameListSerializer, GameDetailSerializer
+from .serializers import PlayerCreateUpdateSerializer ,PlayerListSerializer, PlayerDetailSerializer, GameCreateUpdateSerializer, GameListSerializer, GameDetailSerializer
+from django.shortcuts import get_object_or_404
+
+
+class PlayerCreateAPIView(CreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerCreateUpdateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(player_name=self.request.data['player_name'])
 
 
 class PlayerListAPIView(ListAPIView):
@@ -11,6 +20,20 @@ class PlayerDetailAPIView(RetrieveAPIView):
     queryset = Player.objects.all()
     serializer_class = PlayerDetailSerializer
     lookup_field = 'player_id'
+
+class PlayerDeleteAPIView(DestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerDetailSerializer
+    lookup_field = 'player_id'
+
+
+class GameCreateAPIView(CreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameCreateUpdateSerializer
+
+    def perform_create(self, serializer):
+        player_instance = get_object_or_404(Player, player_id=self.request.data['player_id'])
+        serializer.save(player_id=player_instance)
 
 
 class GameListAPIView(ListAPIView):
